b6f7922b4cb66ad90a59c04e214b49a84153314d
Nixon <voxell.technologies@gmail.com>
2021-07-01 19:46:41 +0800

    first push

---
+++ CherubNLP/Sentence.cs
@@ -0,0 +1,21 @@
+﻿using CherubNLP.Tokenize;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CherubNLP
+{
+    public class Sentence
+    {
+        // User defined id
+        public string Id { get; set; }
+
+        public List<Token> Words { get; set; }
+
+        public String Label { get; set; }
+
+        public String Text { get; set; }
+
+        public double[] Vector { get; set; }
+    }
+}
